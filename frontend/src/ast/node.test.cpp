cfa0eee0866005d01a8cf8499075155ce0df6fb7
Hayaan Hussein Abdi <hayaanhusseinabdi@gmail.com>
2023-07-22 02:48:20 +0000

    Fix some AST node tests

--- frontend/src/ast/node.test.cpp
+++ frontend/src/ast/node.test.cpp
@@ -13,17 +13,19 @@ using Double = frontend::ast::Literal<double>;
 TEST(Node, Literal) {
     auto num = frontend::ast::create_node<Double>(3.14);
 
-    FAIL() << num->to_string();
+    EXPECT_DOUBLE_EQ(num->value_, 3.14);
 }
 
 TEST(Node, UnaryOperation) {
-    auto op = frontend::ast::Operator::Type::ADDITION;
+    auto op = frontend::ast::Operator::Type::UNARY_PLUS;
     auto operand = std::make_unique<Double>(3.15);
 
     auto unary_op = frontend::ast::create_node<frontend::ast::UnaryOperation>(
         op, std::move(operand));
 
-    FAIL() << unary_op->to_string();
+    EXPECT_STREQ(
+        unary_op->to_string().c_str(),
+        "UnaryOperation(operator: UNARY_PLUS, operand: Literal(value: 3.15))");
 }
 
 TEST(Node, BinaryOperation) {
@@ -34,7 +36,9 @@ TEST(Node, BinaryOperation) {
     auto bin_op = frontend::ast::create_node<frontend::ast::BinaryOperation>(
         std::move(left), op, std::move(right));
 
-    FAIL() << bin_op->to_string();
+    EXPECT_STREQ(bin_op->to_string().c_str(),
+                 "BinaryOperation(left: Literal(value: 3.14), operation: "
+                 "ADDITION, right: Literal(value: 3.15))");
 }
 
 } // namespace
